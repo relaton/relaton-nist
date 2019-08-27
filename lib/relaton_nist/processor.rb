e8f7e7fbce339491fb50e84f3deba14cef2eb53a
Andrei Kislichenko <android.2net@gmail.com>
2019-08-27 19:04:52 +0200

    "require" flavor logic fixing #34

---
+++ lib/relaton_nist/processor.rb
@@ -0,0 +1,33 @@
+require "relaton/processor"
+
+module RelatonNist
+  class Processor < Relaton::Processor
+    def initialize
+      @short = :relaton_nist
+      @prefix = "NIST"
+      @defaultprefix = %r{^(NIST|NISTGCR|ITL Bulletin|JPCRD|NISTIR|CSRC)[ /]}
+      @idtype = "NIST"
+    end
+
+    # @param code [String]
+    # @param date [String, NilClass] year
+    # @param opts [Hash]
+    # @return [RelatonNist::GbBibliographicItem]
+    def get(code, date = nil, opts = {})
+      ::RelatonNist::NistBibliography.get(code, date, opts)
+    end
+
+    # @param xml [String]
+    # @return [RelatonNist::GbBibliographicItem]
+    def from_xml(xml)
+      ::RelatonNist::XMLParser.from_xml xml
+    end
+
+    # @param hash [Hash]
+    # @return [RelatonNist::GbBibliographicItem]
+    def hash_to_bib(hash)
+      item_hash = ::RelatonNist::HashConverter.hash_to_bib(hash)
+      ::RelatonNist::NistBibliographicItem.new item_hash
+    end
+  end
+end
