41a3862a7a381cea1e2e5738a054266d789568d2
Andrei Kislichenko <android.2net@gmail.com>
2022-05-01 14:50:43 +0200

    fix test relaton/relaton#99

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+# Encoding.default_external = Encoding::UTF_8
+# Encoding.default_internal = Encoding::UTF_8
 
 source "https://rubygems.org"
 
--- spec/relaton_nist/hit_collection_spec.rb
+++ spec/relaton_nist/hit_collection_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RelatonNist::HitCollection do
   context "update json file" do
     it "when ctime is nil" do
       VCR.use_cassette "json_data" do
-        expect(File).to receive(:exist?).with("/Users/andrej/.relaton/nist/pubs-export.zip").at_most(:once).and_return true
+        expect(File).to receive(:exist?).with(RelatonNist::HitCollection::DATAFILE).at_most(:once).and_return true
         expect(File).to receive(:exist?).and_call_original.at_most(3).times
         expect(File).to receive(:ctime).and_return(nil).at_most(1).time
         result = RelatonNist::NistBibliography.get "SP 800-205 (February 2019) (PD)"
