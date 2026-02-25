955703a8e020ef7eeb6e0a934bce84cd97c66012
Andrei Kislichenko <android.2net@gmail.com>
2026-02-25 13:01:36 -0500

    Update grammar & Get rid of ext/editorialgroup relaton/relaton-bib#103

--- lib/relaton/nist/ext.rb
+++ lib/relaton/nist/ext.rb
@@ -1,26 +1,19 @@
 require_relative "doctype"
-require_relative "editorial_group"
 require_relative "comment_period"
 
 module Relaton
   module Nist
-    class Ext < Lutaml::Model::Serializable
-      attribute :schema_version, :string
+    class Ext < Bib::Ext
+      attribute :schema_version, method: :get_schema_version
       attribute :doctype, Doctype
-      attribute :subdoctype, :string
-      attribute :flavor, :string
-      attribute :editorialgroup, EditorialGroup
-      attribute :ics, Bib::ICS, collection: true
       attribute :commentperiod, CommentPeriod
 
       xml do
-        map_attribute "schema-version", to: :schema_version
-        map_element "doctype", to: :doctype
-        map_element "subdoctype", to: :subdoctype
-        map_element "flavor", to: :flavor
-        map_element "editorialgroup", to: :editorialgroup
-        map_element "ics", to: :ics
         map_element "commentperiod", to: :commentperiod
+      end
+
+      def get_schema_version
+        Relaton.schema_versions["relaton-model-nist"]
       end
     end
   end
